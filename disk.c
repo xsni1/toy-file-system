a91eb346ec2ee3eb994f9f71f818f60becd7d14d
xsni1 <thesniboy@gmail.com>
2024-07-06 22:20:10 +0200

    start implementing real-disk mock

---
+++ disk.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "disk.h"
+
+Disk *disk_open(char* path, size_t blocks) {
+
+}
---
+++ disk.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+typedef struct Disk Disk;
+struct Disk {
+  int fd;
+  size_t blocks;
+};
+
+Disk *disk_open(char *path, size_t blocks);
+void disk_close(Disk *disk);
+
+size_t disk_read(Disk *disk, size_t block, char *data);
+size_t disk_write(Disk *disk, size_t block, char *data);
